bc02d06f2c8da04295ea63eb02b4fc721e80e63f
salabara <bckuo85@gmail.com>
2025-03-08 11:32:18 +0800

    feat: prepare_data.py now can merge dataset groups
    Summary: Add the feature of merging selected dataset groups in prepare_data.py
    Details: This feature make the scaling up the QA more convenient.
    Note: The raw results (csv files) are removed from the repository. The jsonl version will be added later.

--- prepare_data.py
+++ prepare_data.py
@@ -1,14 +1,14 @@
 import os
 import pandas as pd
-import numpy as np  # Add numpy import
 
-from typing import List, Dict, Deque, Tuple
+from typing import List, Dict
 
 ##############################################################################
 # DATASET CONFIG
 ##############################################################################
 PATH_CURRENT = os.path.abspath(os.getcwd())
-PATH_DATASET_JSONL = PATH_CURRENT + "/data/financebench_open_source.jsonl"
+PATH_DATASET_PREFIX = "data/financebench_open_source"
+PATH_DATASET_JSONL = f"{PATH_CURRENT}/{PATH_DATASET_PREFIX}.jsonl"
 PATH_DOCUMENT_INFO_JSONL = PATH_CURRENT + "/data/financebench_document_information.jsonl"
 
 # Choose DATASET PORTION:
@@ -20,51 +20,84 @@ DATASET_PORTION = "OPEN_SOURCE"
 ##############################################################################
 # LOAD DATASET
 ##############################################################################
-# 
+#
 # Set Number of Groups
 N_GROUPS = 5
 
-# Load Full Dataset
-df_questions = pd.read_json(PATH_DATASET_JSONL, lines=True)
-df_questions = df_questions.sort_values("doc_name")
 
-# Split Dataset by Question Type
-categorized_questions = {}
-categorized_questions["metrics-generated"] = df_questions[df_questions["question_type"] == "metrics-generated"]
-categorized_questions["domain-relevant"] = df_questions[df_questions["question_type"] == "domain-relevant"]
-categorized_questions["novel-generated"] = df_questions[df_questions["question_type"] == "novel-generated"]
-
-##############################################################################
-# SPLIT DATAFRAME INTO N EQUAL GROUPS RANDOMLY
-##############################################################################
-
+# Split DataFrame into N Groups Randomly
 def split_dataframe_randomly(df: pd.DataFrame, n: int) -> List[pd.DataFrame]:
-    """
-    Splits a DataFrame into n equal groups randomly using iloc.
-    
-    :param df: The DataFrame to split.
-    :param n: The number of groups to split the DataFrame into.
-    :return: A list of DataFrames.
-    """
+
     df_shuffled = df.sample(frac=1, random_state=0).reset_index(drop=True)
     group_size = len(df) // n
-    groups = [df_shuffled.iloc[i*group_size:(i+1)*group_size] for i in range(n)]
+    groups = [df_shuffled.iloc[i * group_size : (i + 1) * group_size] for i in range(n)]
+
     return groups
 
-# Split DataFrames
-categorized_questions_groups = {}
-for key, df in categorized_questions.items():
-    categorized_questions_groups[key] = split_dataframe_randomly(df, N_GROUPS)
 
 # Merge questions by index
-merged_questions_groups: List[pd.DataFrame] = []
-for i in range(N_GROUPS):
-    merged_questions = pd.concat([categorized_questions_groups[key][i] for key in categorized_questions_groups.keys()])
-    merged_questions.sample(frac=1, random_state=0).reset_index(drop=True)
-    merged_questions_groups.append(merged_questions)
+def merge_dataframes(categorized_questions_groups: Dict[str, List[pd.DataFrame]], n: int) -> List[pd.DataFrame]:
 
-# Save DataFrames
-for i, df in enumerate(merged_questions_groups):
-    df.to_json(f"data/financebench_open_source_group_{i}.jsonl", orient="records", lines=True)
+    merged_questions_groups: List[pd.DataFrame] = []
+
+    for i in range(n):
+        merged_questions = pd.concat([categorized_questions_groups[key][i] for key in categorized_questions_groups.keys()])
+        merged_questions.sample(frac=1, random_state=0).reset_index(drop=True)
+        merged_questions_groups.append(merged_questions)
+
+    return merged_questions_groups
 
 
+def split_and_save_dataframe():
+
+    # Load Full Dataset
+    df_questions = pd.read_json(PATH_DATASET_JSONL, lines=True)
+    df_questions = df_questions.sort_values("doc_name")
+
+    # Split Dataset by Question Type
+    categorized_questions = {}
+
+    categorized_questions["metrics-generated"] = df_questions[df_questions["question_type"] == "metrics-generated"]
+    categorized_questions["domain-relevant"] = df_questions[df_questions["question_type"] == "domain-relevant"]
+    categorized_questions["novel-generated"] = df_questions[df_questions["question_type"] == "novel-generated"]
+
+    # Split DataFrames
+    categorized_questions_groups: Dict[str, List[pd.DataFrame]] = {}
+
+    for key, df in categorized_questions.items():
+        categorized_questions_groups[key] = split_dataframe_randomly(df, N_GROUPS)
+
+    # Merge DataFrames
+    merged_questions_groups = merge_dataframes(categorized_questions_groups, N_GROUPS)
+
+    # Save DataFrames
+    for i, df in enumerate(merged_questions_groups):
+        df.to_json(f"{PATH_DATASET_PREFIX}_group_{i}.jsonl", orient="records", lines=True)
+
+
+def load_dataset() -> List[pd.DataFrame]:
+
+    path_dataset_partial = [f"{PATH_CURRENT}/{PATH_DATASET_PREFIX}_group_{i}.jsonl" for i in range(N_GROUPS)]
+    df_question_groups = [pd.read_json(path_dataset_partial[i], lines=True) for i in range(N_GROUPS)]
+
+    return df_question_groups
+
+
+def merge_selected_groups(selected_groups: List[int]):
+
+    df_question_groups = load_dataset()
+
+    postfix = "".join([str(i) for i in sorted(selected_groups)])
+
+    df_question_groups = pd.concat([df_question_groups[i] for i in selected_groups])
+    df_question_groups.to_json(f"{PATH_DATASET_PREFIX}_group_{postfix}.jsonl", orient="records", lines=True)
+
+
+def main():
+    
+    selected_groups = [1, 2, 3, 4]
+    merge_selected_groups(selected_groups)
+
+
+if __name__ == "__main__":
+    main()
